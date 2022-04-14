b225b545ef8cbfa47c698bb9733e982f13f0a610
Rodolfo Campos <camposer@gmail.com>
2022-04-14 14:50:41 +0200

    Improve tests (#11)

--- errbot_slack_bolt_backend/test_slackbolt_resolve_access_form_bot_id.py
+++ errbot_slack_bolt_backend/test_slackbolt_resolve_access_form_bot_id.py
@@ -6,8 +6,6 @@ from .test_common import SlackBoltBackendConfig
 bot_id = '123'
 
 class Test_resolve_access_form_bot_id:
-    wrong_bot_id = None
-
     @pytest.fixture
     def mocked_backend(self):
         return inject_mocks()
@@ -16,11 +14,6 @@ class Test_resolve_access_form_bot_id:
         mocked_backend.username_to_bot_id = MagicMock(return_value=bot_id)
         mocked_backend.resolve_access_form_bot_id()
         assert mocked_backend.bot_config.ACCESS_FORM_BOT_INFO.get('bot_id') == bot_id 
-
-    def test_not_resolve_access_form_bot_id(self, mocked_backend):
-        mocked_backend.username_to_bot_id = MagicMock(return_value=self.wrong_bot_id)
-        mocked_backend.resolve_access_form_bot_id()
-        assert mocked_backend.bot_config.ACCESS_FORM_BOT_INFO.get('bot_id') == self.wrong_bot_id 
 
 def inject_mocks():
     backend = SlackBoltBackend(SlackBoltBackendConfig())
